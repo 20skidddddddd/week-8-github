6a0af80f0d224c318634d23d507c443e90f804c0
Bibushan Bahadur Singh <bibushansingh14@gmail.com>
2025-01-12 08:13:12 +0545

    Student File

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student{
+    String name;
+    int age;
+
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+}
